04545d6613bc3725e8a30480d8f9ae7c34506067
Rutvik Kumar <rutvikvankar08@gmail.com>
2025-09-21 20:43:38 +0530

    add more lines

--- mynumpy.py
+++ mynumpy.py
@@ -4,12 +4,24 @@
 
 # print(arr)
 
+# import numpy as np
+
+# arr = np.array([1, 2, 3, 4, 5])
+
+# print(arr)
+
+# print(np.__version__)
+
+# print(type(arr))        
+
 import numpy as np
 
-arr = np.array([1, 2, 3, 4, 5])
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
 
-print(arr)
-
-print(np.__version__)
-
-print(type(arr))        
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)
